bb7848553f3387c55f6328efe927d9ecdc5c19e2
akaitigo <ryusei1116nakayama@outlook.com>
2021-09-05 00:02:12 +0900

    13 1kaime

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,7 @@
 function getData() {
+    let arrowFunction = () => {
     // Arrowという文字列を返す処理をアロー関数を入れて書きましょう。
-    return // ここにアロー関数の処理を書く
+    return "Arrow"
+    }
+    return arrowFunction();
 }
