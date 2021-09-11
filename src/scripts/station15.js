660939f1872db26178f586132495af62e711dc06
akaitigo <ryusei1116nakayama@outlook.com>
2021-09-11 15:52:46 +0900

    14 1kaime

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,22 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
+    { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },//配列ヨウ素1
+    { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }//配列ヨウ素２
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
-}
-
+function test(userList) {
+ promise = new Promise((resolve) => {
+   setTimeout(() => {
+     const result = userList.map((data) =>{
+      data["full_name"] = data.family_name+" "+data.first_name;
+      return data;
+     });
+     resolve(result);
+   },3000);
+ });
+ return promise;
+} 
